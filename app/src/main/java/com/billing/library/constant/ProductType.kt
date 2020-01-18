902c26708838c6e352972c0a39966751338aabd4
vetsb <dmitriylebyodkin@gmail.com>
2020-01-19 01:28:07 +0300

    Removed old library's files

--- app/src/main/java/com/billing/library/constant/ProductType.kt
+++
@@ -1,7 +0,0 @@
-package com.billing.library.constant
-
-enum class ProductType {
-
-    INAPP,
-    SUBS
-}
--- app/src/main/java/com/billing/library/constant/PurchaseStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.billing.library.constant
-
-enum class PurchaseStatus {
-
-    PURCHASED,
-    NOT_PURCHASED
-}
